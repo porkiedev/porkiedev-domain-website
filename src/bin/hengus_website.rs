93e0ffc92f74ee972f66a0a54945470f04460f14
porkiedev <porkiedev@gmail.com>
2025-01-08 23:37:46 -0600

    Hengus Commit

---
+++ src/bin/hengus_website.rs
@@ -0,0 +1,43 @@
+//
+// Contains the code for the hengus subdomain
+//
+
+use actix_files::{Files, NamedFile};
+use actix_web::{get, App, HttpServer, Responder};
+use tracing::{info, Level};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+/// Serve the sus hengus path
+#[get("/")]
+async fn index() -> impl Responder {
+    // Respond with the index file
+    NamedFile::open_async("./assets/hengus/index.html").await
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Create a filter that limits the log level to TRACE for this module, and INFO for everything else
+    let filter = tracing_subscriber::filter::Targets::new()
+        .with_default(Level::INFO)
+        .with_target("module", Level::TRACE)
+        .with_target(module_path!(), Level::TRACE);
+    // Initialize the logger
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(filter)
+        .init();
+
+    // Initialize the server
+    info!("Actix server is starting");
+    HttpServer::new(|| {
+        App::new()
+            // Include the logger middleware
+            .wrap(web::Logger)
+            .service(index)
+            .service(Files::new("/", "assets/hengus"))
+    })
+        .bind(("0.0.0.0", 80))?
+        .run()
+        .await
+}
---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+//
+// Contains code used by multiple binaries
+//
+
+use actix_web::dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform};
+use actix_web::Error;
+use std::future::{ready, Ready};
+use futures_util::future::LocalBoxFuture;
+use tracing::info;
+
+/// The request logging middleware
+pub struct Logger;
+impl <S, B> Transform<S, ServiceRequest> for Logger
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = LoggerMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(LoggerMiddleware { service }))
+    }
+}
+
+/// The inner part of the request logging middleware
+pub struct LoggerMiddleware<S> {
+    service: S
+}
+impl<S, B> Service<ServiceRequest> for LoggerMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        tracing::info!("Incoming request");
+
+        // Get the request headers
+        let headers = req.headers();
+        let mut log_string = String::new();
+
+        // Try to resolve the client's IP address
+        // NOTE: Checks for a cloudflare proxy header first (cf-connecting-ip),
+        // and then checks for direct client address
+        let client_ip_address = || {
+            if let Some(client_ip) = headers.get("cf-connecting-ip") {
+                return format!("{:?}", client_ip);
+            }
+            if let Some(client_ip) = req.peer_addr() {
+                return format!("{}", client_ip);
+            }
+            String::from("unknown-ip")
+        };
+
+        // Format the string
+        log_string.push_str(
+            &format!("Incoming connection from '{}'\nRequest Headers:\n",
+                     client_ip_address()
+            )
+        );
+
+        // Push every request header to the string
+        for header in headers {
+            log_string.push_str(&format!("  {}: {:?}\n", header.0, header.1));
+        }
+
+        // Log the HTTP request
+        info!("{log_string}");
+
+        // Call the next service
+        let fut = self.service.call(req);
+
+        // Wait for the request to complete
+        Box::pin(async move {
+            let res = fut.await?;
+            Ok(res)
+        })
+    }
+}
